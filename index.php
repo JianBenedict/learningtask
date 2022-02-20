97a1e7227356c7ad726714a1f67b08d40a5920ef
JianBenedict <100068737+JianBenedict@users.noreply.github.com>
2022-02-20 01:46:51 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+ 
+ <head>
+ 
+ <title>My Home Page </title>
+ </head>
+ 
+ <body>
+ 
+ <h1> Welcome to My Website! I am Jian Benedict S. Gapulan! </h1>
+ 
+ <ul>
+ <li>a<a href= "index.php">HOME</a></li>
+ <li>a<a href= "/LT2/index.html">LEARNING TASK 2</a></li>
+ </ul>
+ 
+ </body>
+ </html>
